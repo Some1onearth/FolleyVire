c7528b0d2eec8ad103efbea50d4f875756ff8250
Some1onearth <chris_lee@outlook.com.au>
2022-12-01 23:28:40 +1100

    Added random song playing

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Audio : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<AudioClip> soundtrack;
+    AudioSource auxSauce;
+    private void Awake()
+    {
+        auxSauce = gameObject.GetComponent<AudioSource>();
+    }
     void Start()
     {
-        
+        if (!auxSauce.playOnAwake)
+        {
+            //random song choosing
+            auxSauce.clip = soundtrack[Random.Range(0, soundtrack.Count)];
+            auxSauce.Play();
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (!auxSauce.isPlaying)
+        {
+            //changes song after a song ends
+            auxSauce.clip = soundtrack[Random.Range(0, soundtrack.Count)];
+            auxSauce.Play();
+        }
     }
 }
